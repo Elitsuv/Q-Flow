746c72d3edbb6c087a3e0dfc5ce3308426688f3e
Elitsuv <work.for.jeez999@gmail.com>
2024-06-01 23:21:28 +0530

    #2 Added Dedicated Dictionary to get patient or user data

--- core/queues.py
+++ core/queues.py
@@ -1,5 +1,15 @@
-class queue:
-    def get_data():
-        name = input("Enter the patient name: ")
-        age = int("Enter age: ")
-        issue = input("Enter the patient problem:")
+def get_data():
+    return input("Enter Name: ")
+
+name = get_data()
+while True:  
+    try:
+      age = int(input("Enter age: "))
+      break  
+    except ValueError:
+      print("Invalid age. Please enter a number.")
+
+issue = input("Enter the patient problem: ")
+
+patient_data = {"name": name, "age": age, "issue": issue}
+print(patient_data)
